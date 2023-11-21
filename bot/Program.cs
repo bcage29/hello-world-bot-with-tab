caf6091ab14a029224aafd178820f34cafbbaa0f
Brennen Cage <brennen.cage@microsoft.com>
2023-11-21 10:14:47 -0500

    Created notification bot

--- bot/Program.cs
+++ bot/Program.cs
@@ -1,28 +1,57 @@
-using System.Collections.Concurrent;
-using bot;
-using bot.Bots;
+using NotificationBot;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Bot.Connector.Authentication;
-using Microsoft.Bot.Schema;
+using Microsoft.TeamsFx.Conversation;
+using NotificationBot.Bots;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
-
-// Create the Bot Framework Authentication to be used with the Bot Adapter.
-builder.Services.AddSingleton<BotFrameworkAuthentication, ConfigurationBotFrameworkAuthentication>();
-
-// Create the Bot Framework Adapter with error handling enabled.
-builder.Services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
-
-// Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
-builder.Services.AddTransient<IBot, TeamsConversationBot>();
+builder.Services.AddHttpClient("WebClient", client => client.Timeout = TimeSpan.FromSeconds(600));
+builder.Services.AddHttpContextAccessor();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//
+// Currently not used, but can be if you need to set the
+// Bot Framework AppId and AppPassword programatically.
+//
+// Prepare Configuration for ConfigurationBotFrameworkAuthentication
+// var config = builder.Configuration.Get<ConfigOptions>();
+// builder.Configuration["MicrosoftAppType"] = "MultiTenant";
+// builder.Configuration["MicrosoftAppId"] = config.BOT_ID;
+// builder.Configuration["MicrosoftAppPassword"] = config.BOT_PASSWORD;
+
+// Create the Bot Framework Authentication to be used with the Bot Adapter.
+builder.Services.AddSingleton<BotFrameworkAuthentication, ConfigurationBotFrameworkAuthentication>();
+
+// Create the Cloud Adapter with error handling enabled.
+// Note: some classes expect a BotAdapter and some expect a BotFrameworkHttpAdapter, so
+// register the same adapter instance for all types.
+builder.Services.AddSingleton<CloudAdapter, AdapterWithErrorHandler>();
+builder.Services.AddSingleton<IBotFrameworkHttpAdapter>(sp => sp.GetService<CloudAdapter>()!);
+builder.Services.AddSingleton<BotAdapter>(sp => sp.GetService<CloudAdapter>()!);
+
+// Create the Conversation with notification feature enabled.
+builder.Services.AddSingleton(sp =>
+{
+    var options = new ConversationOptions()
+    {
+        Adapter = sp.GetService<CloudAdapter>(),
+        Notification = new NotificationOptions
+        {
+            BotAppId = builder.Configuration["MicrosoftAppType"]
+        },
+    };
+
+    return new ConversationBot(options);
+});
+
+// Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
+builder.Services.AddTransient<IBot, TeamsBot>();
 
 var app = builder.Build();
 
@@ -31,7 +60,10 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
+
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
