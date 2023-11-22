ca8a657e4cc29e36d28aa7e61e3ae40a972b01ed
Brennen Cage <brennen.cage@microsoft.com>
2023-11-22 11:00:10 -0500

    added SSO support and hot reloading for dotnet app

--- bot/Program.cs
+++ bot/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Bot.Connector.Authentication;
 using Microsoft.TeamsFx.Conversation;
 using NotificationBot.Bots;
+using Microsoft.TeamsFx.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,15 +16,11 @@ builder.Services.AddHttpContextAccessor();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//
-// Currently not used, but can be if you need to set the
-// Bot Framework AppId and AppPassword programatically.
-//
 // Prepare Configuration for ConfigurationBotFrameworkAuthentication
-// var config = builder.Configuration.Get<ConfigOptions>();
-// builder.Configuration["MicrosoftAppType"] = "MultiTenant";
-// builder.Configuration["MicrosoftAppId"] = config.BOT_ID;
-// builder.Configuration["MicrosoftAppPassword"] = config.BOT_PASSWORD;
+var config = builder.Configuration.Get<ConfigOptions>();
+builder.Configuration["MicrosoftAppType"] = "MultiTenant";
+builder.Configuration["MicrosoftAppId"] = config.BOT_ID;
+builder.Configuration["MicrosoftAppPassword"] = config.BOT_PASSWORD;
 
 // Create the Bot Framework Authentication to be used with the Bot Adapter.
 builder.Services.AddSingleton<BotFrameworkAuthentication, ConfigurationBotFrameworkAuthentication>();
@@ -35,23 +32,49 @@ builder.Services.AddSingleton<CloudAdapter, AdapterWithErrorHandler>();
 builder.Services.AddSingleton<IBotFrameworkHttpAdapter>(sp => sp.GetService<CloudAdapter>()!);
 builder.Services.AddSingleton<BotAdapter>(sp => sp.GetService<CloudAdapter>()!);
 
-// Create the Conversation with notification feature enabled.
-builder.Services.AddSingleton(sp =>
-{
-    var options = new ConversationOptions()
-    {
-        Adapter = sp.GetService<CloudAdapter>(),
-        Notification = new NotificationOptions
-        {
-            BotAppId = builder.Configuration["MicrosoftAppType"]
-        },
-    };
+// Create the Bot Framework Adapter with error handling enabled.                                        
+builder.Services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
 
-    return new ConversationBot(options);
-});
+// Create the storage we'll be using for User and Conversation state, as well as Single Sign On.
+// (Memory is great for testing purposes.)
+builder.Services.AddSingleton<IStorage, MemoryStorage>();
+
+// Create the User state. (Used in this bot's Dialog implementation.)
+builder.Services.AddSingleton<UserState>();
+
+// Create the Conversation state. (Used by the Dialog system itself.)
+builder.Services.AddSingleton<ConversationState>();
+
+// The Dialog that will be run by the bot.
+builder.Services.AddSingleton<SsoDialog>();
 
 // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
-builder.Services.AddTransient<IBot, TeamsBot>();
+builder.Services.AddTransient<IBot, TeamsSsoBot<SsoDialog>>();
+
+builder.Services.AddOptions<BotAuthenticationOptions>().Configure(options =>
+{
+  options.ClientId = config.TeamsFx.Authentication.ClientId;
+  options.ClientSecret = config.TeamsFx.Authentication.ClientSecret;
+  options.OAuthAuthority = config.TeamsFx.Authentication.OAuthAuthority;
+  options.ApplicationIdUri = config.TeamsFx.Authentication.ApplicationIdUri;
+  options.InitiateLoginEndpoint = config.TeamsFx.Authentication.Bot.InitiateLoginEndpoint;
+});
+
+builder.Services.AddSingleton(sp =>
+{
+  var options = new ConversationOptions()
+  {
+    Adapter = sp.GetService<CloudAdapter>(),
+    Notification = new NotificationOptions
+    {
+        BotAppId = config.BOT_ID,
+        //Store = CustomStorageStore
+        //This needs to be an implementation of IConversationReferenceStore
+    }
+  };
+
+  return new ConversationBot(options);
+});
 
 var app = builder.Build();
 
